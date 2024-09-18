c558d15324089cb5a593da4c041264fa9e2f84ed
Puspanjali14 <neupanepuspanjali@gmail.com>
2024-09-19 02:37:34 +0545

    updated

--- signup.js
+++ signup.js
@@ -5,7 +5,6 @@ document.getElementById('signupForm').addEventListener('submit', function(event)
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
 
-    // Here you can handle the signup logic, such as validating the inputs, sending them to a server, etc.
     console.log('Form submitted:', { name, email, password });
 
     alert("Account Created Successfully!");
