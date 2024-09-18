9bb4529fff921b766318b79dde509573f1d69252
Puspanjali14 <neupanepuspanjali@gmail.com>
2024-09-18 14:47:10 +0545

    updated

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
-let lastScrollY = window.scrollY;
-const header = document.getElementById('header');
-const navLinks = document.querySelector('.nav-links');
 
-// Scrolling header behavior
-window.addEventListener('scroll', () => {
-  if (window.scrollY > lastScrollY) {
-    // Scrolling down, hide header
-    header.classList.add('hidden');
+    let lastScrollY = window.scrollY;
+    const header = document.getElementById('header');
+
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > lastScrollY) {
+        // Scrolling down, hide header
+        header.classList.add('hidden');
+      } else {
+        // Scrolling up, show header
+        header.classList.remove('hidden');
+      }
+      lastScrollY = window.scrollY;
+    });
+
+
+    
+document.addEventListener('scroll', function() {
+  var footer = document.querySelector('footer');
+  var scrollPosition = window.scrollY + window.innerHeight;
+  var pageHeight = document.documentElement.scrollHeight;
+
+  if (scrollPosition >= pageHeight) {
+    footer.style.display = 'block'; // Show footer when at the bottom
   } else {
-    // Scrolling up, show header
-    header.classList.remove('hidden');
+    footer.style.display = 'none'; // Hide footer when not at the bottom
   }
-  lastScrollY = window.scrollY;
 });
