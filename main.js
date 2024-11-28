1454712329ba7b3967ae2393074b2f3383bff5a3
Puspanjali14 <neupanepuspanjali@gmail.com>
2024-11-28 19:32:32 +0545

    updated

---
+++ main.js
@@ -0,0 +1,9 @@
+// script.js
+document.querySelector('.new-resume-btn').addEventListener('click', () => {
+    alert('Redirecting to build a new resume...');
+  });
+  
+  document.querySelector('.build-resume-btn').addEventListener('click', () => {
+    alert('Let’s start building your first resume!');
+  });
+  
